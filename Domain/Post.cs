f0994adadf90152375185fb1e208d2b19ee622e0
Nafets94 <effsty@gmail.com>
2023-06-08 20:52:38 +0300

    added the Post entity

---
+++ Domain/Post.cs
@@ -0,0 +1,29 @@
+﻿namespace Domain
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+
+        public Guid AuthorId { get; set; }
+
+        public Guid ParentId { get; set; }
+
+        public string Title { get; set; }
+
+        public string MetaTitle { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Summary { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public DateTime PublishedAt { get; set; }
+
+        public string Content { get; set; }
+    }
+}
