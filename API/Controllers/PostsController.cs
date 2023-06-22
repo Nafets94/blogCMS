820de9cec5c1c1c2a473a0b2436e8f3c5ce3a2ea
Nafets94 <effsty@gmail.com>
2023-06-22 21:23:59 +0300

    added Details handler

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -15,7 +15,7 @@ namespace API.Controllers
         [HttpGet("{id}")] //api/posts/{guid}
         public async Task<ActionResult<Post>> GetPost(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query { Id = id });
         }
     }
 }
