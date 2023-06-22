38d750779cfcff58c8c24925cb489ae9cf16f87c
Nafets94 <effsty@gmail.com>
2023-06-22 20:53:25 +0300

    reduced controller size

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -1,32 +1,21 @@
 ﻿using Application.Posts;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Persistence;
 
 namespace API.Controllers
 {
     public class PostsController : BaseApiController
     {
-        private readonly IMediator _mediator;
-        private readonly DataContext _context;
-
-        public PostsController(IMediator mediator, DataContext context)
-        {
-            _mediator = mediator;
-            _context = context;
-        }
-
         [HttpGet] //api/posts
         public async Task<ActionResult<List<Post>>> GetPosts()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] //api/posts/{guid}
         public async Task<ActionResult<Post>> GetPost(Guid id)
         {
-            return await _context.Posts.FindAsync(id);
+            return Ok();
         }
     }
 }
